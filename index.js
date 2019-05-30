5b567e67aae2a50aa1c6dba38ef825202f09d778
Monica Lee <mylee.715@gmail.com>
2019-05-30 19:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ function happyHolidays() {
   return "Happy Holidays!";
 }
 
-function happyHolidayTo(name) {
+function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!";
 }
 
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}";
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}";
+}
